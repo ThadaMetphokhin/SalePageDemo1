50032f229e2d6f638faf8d202e67a2872d29522a
changthai <thadametphokhin@gmail.com>
2026-03-31 17:18:57 +0700

    change name icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,9 +7,9 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="icon" href="/icondemo.ico" sizes="any">
-    <link rel="icon" href="/icondemo.svg" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="/icondemo.png">
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/favicon.png">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
